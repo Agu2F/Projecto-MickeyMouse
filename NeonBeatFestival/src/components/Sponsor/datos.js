f765baad43b474af56f76f0e4051fb4dfd39be8f
A2F <agustin.alvarezfijo@gmail.com>
2025-02-21 19:28:17 +0100

    Componente Sponsor hecho

---
+++ NeonBeatFestival/src/components/Sponsor/datos.js
@@ -0,0 +1,11 @@
+export default [
+    { name: "Cruzcampo", image: "../public/imagenes/cruzcampo.png" },
+    { name: "Five Guys", image: "../public/imagenes/FiveGuys.png" },
+    { name: "Coca-Cola", image: "../public/imagenes/CocaCola.png" },
+    { name: "Telepizza", image: "../public/imagenes/telepizza.png" },
+    { name: "Burger King", image: "../public/imagenes/burguerking.png" },
+    { name: "Netflix", image: "../public/imagenes/netflix.png" },
+    { name: "YouTube", image: "../public/imagenes/youtube.png" },
+    { name: "Comunismo", image: "../public/imagenes/comunismo.png" },
+];
+
